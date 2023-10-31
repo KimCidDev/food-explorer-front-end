a4e1dc1162b8a1f08a3b1de06c8981942a9ac89d
Kim <cid.kim@gmail.com>
2023-10-30 22:42:36 -0600

    using logo svg signUp page

--- foodExplorer/src/Pages/SignUp/styles.js
+++ foodExplorer/src/Pages/SignUp/styles.js
@@ -3,9 +3,15 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   background-color: ${({ theme }) => theme.COLORS.TINT_TOMATO_100};
 
   h1 {
+    margin-left: 16px;
     color: ${({ theme }) => theme.COLORS.LIGHT_400};
     font-family: 'Roboto Slab', serif;
   }
