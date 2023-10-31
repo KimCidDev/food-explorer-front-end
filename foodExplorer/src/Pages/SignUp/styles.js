8eda40ae9cc68145749f619c282aee9441226032
Kim <cid.kim@gmail.com>
2023-10-30 22:28:38 -0600

    fonts added to index.html

--- foodExplorer/src/Pages/SignUp/styles.js
+++ foodExplorer/src/Pages/SignUp/styles.js
@@ -3,5 +3,10 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
-  background-color: ${({theme}) => theme.COLORS.TINT_TOMATO_100};
+  background-color: ${({ theme }) => theme.COLORS.TINT_TOMATO_100};
+
+  h1 {
+    color: ${({ theme }) => theme.COLORS.LIGHT_400};
+    font-family: 'Roboto Slab', serif;
+  }
 `;
