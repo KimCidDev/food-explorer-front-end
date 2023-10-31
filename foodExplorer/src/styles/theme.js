6dc02d611c9e18989f4b61393f177582a3405931
Kim <cid.kim@gmail.com>
2023-10-30 22:12:28 -0600

    project colors added to theme

--- foodExplorer/src/styles/theme.js
+++ foodExplorer/src/styles/theme.js
@@ -1,36 +1,36 @@
 export default {
   COLORS: {
-    LIGHT_100: '#FFF',
-    LIGHT_200: '#FFF',
-    LIGHT_300: '#FFF',
-    LIGHT_400: '#FFF',
-    LIGHT_500: '#FFF',
-    LIGHT_600: '#FFF',
-    LIGHT_700: '#FFF',
+    LIGHT_100: '#FFFFFF',
+    LIGHT_200: '#FFFAF1',
+    LIGHT_300: '#E1E1E6',
+    LIGHT_400: '#C4C4CC',
+    LIGHT_500: '#7C7C8A',
+    LIGHT_600: '#76797B',
+    LIGHT_700: '#4D585E',
 
-    DARK_100: '#000',
-    DARK_200: '#000',
-    DARK_300: '#000',
-    DARK_400: '#000',
-    DARK_500: '#000',
-    DARK_600: '#000',
-    DARK_700: '#000',
-    DARK_800: '#000',
-    DARK_900: '#000',
-    DARK_1000: '#000',
+    DARK_100: '#000405',
+    DARK_200: '#00070A',
+    DARK_300: '#000204',
+    DARK_400: '#000A0F',
+    DARK_500: '#000C12',
+    DARK_600: '#00111A',
+    DARK_700: '#001119',
+    DARK_800: '#0D161B',
+    DARK_900: '#0D1D25',
+    DARK_1000: '#192227',
 
     GRADIENT_1: '#568182',
     GRADIENT_1: '#571154',
 
-    TINT_TOMATO_100: '#700000',
-    TINT_TOMATO_200: '#700000',
-    TINT_TOMATO_300: '#700000',
-    TINT_TOMATO_400: '#700000',
+    TINT_TOMATO_100: '#750310',
+    TINT_TOMATO_200: '#92000E',
+    TINT_TOMATO_300: '#AB222E',
+    TINT_TOMATO_400: '#AB4D55',
 
-    TINT_CARROT: '#700000',
-    TINT_MINT: '#700000',
+    TINT_CARROT: '#FBA94C',
+    TINT_MINT: '#04D361',
 
-    TINT_CAKE_200: '#700000',
-    TINT_CAKE_100: '#700000'
+    TINT_CAKE_200: '#82F3FF',
+    TINT_CAKE_100: '#065E7C'
   }
 };
