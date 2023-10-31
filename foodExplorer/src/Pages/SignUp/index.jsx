403200aad8aa4eebb1dfa1c53698cec7960644b1
Kim <cid.kim@gmail.com>
2023-10-30 23:03:54 -0600

    testing styles.js

--- foodExplorer/src/Pages/SignUp/index.jsx
+++ foodExplorer/src/Pages/SignUp/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Container } from './styles';
+import { Container, Form } from './styles';
 
 export function SignUp() {
   const [count, setCount] = useState(0)
@@ -7,11 +7,22 @@ export function SignUp() {
   return (
     <Container>
 
+    <Form>
+
       <svg width="39" height="44" viewBox="0 0 39 44" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M19.6574 0L38.4133 10.8287V32.4862L19.6574 43.3149L0.901548 32.4862V10.8287L19.6574 0Z" fill="#065E7C"/>
       </svg>
       <h1> Food Explorer </h1>
-      
+    
+      <p>nome</p>
+      <input type="text" />
+      <p>Email</p>
+      <input type="text" />
+      <p>Password</p>
+      <input type="text" />
+
+
+    </Form>
     </Container>
     
   )
--- foodExplorer/src/Pages/SignUp/styles.js
+++ foodExplorer/src/Pages/SignUp/styles.js
@@ -9,10 +9,34 @@ export const Container = styled.div`
   justify-content: center;
 
   background-color: ${({ theme }) => theme.COLORS.TINT_TOMATO_100};
+`;
 
-  h1 {
+export const Form = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  > h1 {
     margin-left: 16px;
     color: ${({ theme }) => theme.COLORS.LIGHT_400};
     font-family: 'Roboto Slab', serif;
+  }
+
+  > p {
+    margin-top: 24px;
+    margin-bottom: 8px;
+  }
+
+  > input {
+    max-width: 350px;
+    height: 32px;
+    
+    background-color: ${({ theme }) => theme.COLORS.LIGHT_400};
+    border: none;
+    border-radius: 10px;
   }
 `;
