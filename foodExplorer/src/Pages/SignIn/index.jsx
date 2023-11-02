025590884ecabbade73a4dca81759b62704969a9
Kim <cid.kim@gmail.com>
2023-11-01 20:42:59 -0600

    SignIn Page mobile done

---
+++ foodExplorer/src/Pages/SignIn/index.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { Container, Form } from './styles';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+import { ButtonText } from '../../components/ButtonText';
+
+export function SignIn() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Container>
+      <div className="logo">
+      <svg width="39" height="44" viewBox="0 0 39 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M19.6574 0L38.4133 10.8287V32.4862L19.6574 43.3149L0.901548 32.4862V10.8287L19.6574 0Z" fill="#065E7C"/>
+      </svg>
+      <h1> food Explorer </h1>
+      </div>
+    <Form>
+
+    
+      <Input title="Email"placeholder="Ex.: something@thatmail.com"/>
+      <Input title="Senha"placeholder="Six digits or more"/>
+
+      <Button title="Entrar" />
+      <ButtonText title="Criar uma conta"/>
+    </Form>
+    </Container>
+    
+  )
+}
---
+++ foodExplorer/src/Pages/SignIn/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding-top: 80px;
+
+  background-color: ${({ theme }) => theme.COLORS.DARK_400};
+
+  > .logo {
+    display: flex;
+    align-items: center;
+    //padding-top: 160px;
+
+    h1 {
+      margin-left: 10px;
+      color: ${({ theme }) => theme.COLORS.LIGHT_400};
+      font-family: 'Roboto Slab', sans-serif;
+    }
+  }
+`;
+
+export const Form = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  padding-top: 75px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
