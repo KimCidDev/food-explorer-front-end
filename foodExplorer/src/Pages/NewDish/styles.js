feaf9323b20f28e514c14fc117158881d125d1a1
Kim <cid.kim@gmail.com>
2023-11-19 13:37:13 -0700

    newDish page 1st structure

--- foodExplorer/src/Pages/NewDish/styles.js
+++ foodExplorer/src/Pages/NewDish/styles.js
@@ -4,8 +4,6 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
 
-  overflow-y: auto;
-
   background-color: ${({ theme }) => theme.COLORS.DARK_400};
 
   > div:first-child {
@@ -25,7 +23,7 @@ export const Container = styled.div`
   }
 
   > .section {
-    padding: 0 56px;
+    padding: 0 24px;
   }
 
   > .section .childrenBox {
@@ -34,59 +32,15 @@ export const Container = styled.div`
     align-items: center;
 
     height: 100%;
-  }
-
-  > .section img {
-    height: 245px;
-    width: 245px;
-  }
-
-
-> .section h1 {
-  color: ${({ theme }) => theme.COLORS.LIGHT_200};
-  font-size: 27px;
-  display: flex;
-  align-items: center;
-
-  svg {
-    font-size: 12px;
-  }
-}
-
-> .section p {
-  text-align: center;
-}
-
- > .section .tagsSection {
-    display: flex;
-    justify-content: space-evenly;
-    width: 100%;
-    max-width: 450px;
-    flex-wrap: wrap;
-
-  }
-
- > .section .addToBasket {      
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    width: 100%;
-    max-width: 450px;
-    margin-top: 24px;
-
-    > .howManyBox {
-      display: flex;
-      gap: 15px;
-      margin-right: 16px;
-
-      svg, p {
-        font-size: 22px;
-      }
-    }
 
     > button {
       max-width: auto;
     }
+  }
 
+  > .section h1 {
+    color: ${({ theme }) => theme.COLORS.LIGHT_200};
+    font-size: 27px;
+    display: flex;
   }
 `;
