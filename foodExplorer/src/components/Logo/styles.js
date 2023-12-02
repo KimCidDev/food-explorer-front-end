ba79d368287d2518535acb0e3319b14865d0d327
Kim <cid.kim@gmail.com>
2023-12-02 02:43:02 -0700

    Logo component admin version

--- foodExplorer/src/components/Logo/styles.js
+++ foodExplorer/src/components/Logo/styles.js
@@ -1,16 +1,25 @@
 import styled from 'styled-components';
+import { DEVICE_BREAKPOINTS } from '../../styles/deviceBreakpoints';
 
 export const Container = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-start;
+  gap: 7px;
 
   .adminBox {
     display: flex;
     flex-direction: column;
+    justify-content: end;
+  }
+
+  .adminBox p {
+    text-align: right;
+    font-size: 0.7rem;
+    color: ${({ theme }) => theme.COLORS.TINT_CAKE_200};
   }
 
   h1 {
-    margin-left: 10px;
+    // margin-left: 10px;
     color: ${({ theme }) => theme.COLORS.LIGHT_400};
     font-family: 'Roboto Slab', sans-serif;
     letter-spacing: -1px;
