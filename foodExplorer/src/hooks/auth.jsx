ba8e62876e7c009d966ecf9d0af2f55c0f62accd
Kim <cid.kim@gmail.com>
2024-01-30 06:11:50 -0700

    feat: useEffect to use data saved on localStorage

--- foodExplorer/src/hooks/auth.jsx
+++ foodExplorer/src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import { api } from '../services/api'
 
 export const AuthContext = createContext({});
@@ -26,6 +26,8 @@ function AuthProvider({ children }) {
     }
 
   }
+
+  
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
