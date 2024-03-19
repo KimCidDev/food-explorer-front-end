efb86274d1e5a5929901852da80b1b5cc0cb0857
Kim <cid.kim@gmail.com>
2024-03-18 18:29:33 -0600

    build: fix on vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     manifest: true,
     rollupOptions: {
-      input: '/foodExplorer/main.js' // Adjust the path to your main JavaScript file
+      input: 'src/main.jsx' // Adjust the path to your new entry point
     }
   }
 });
