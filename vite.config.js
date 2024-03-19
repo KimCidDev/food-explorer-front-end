bd6e7069ef1b9a0e751b45648b8804414683b991
Kim <cid.kim@gmail.com>
2024-03-19 17:38:10 -0600

    build: vite deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-
 export default defineConfig({
   plugins: [react()],
+  root: 'src',
   build: {
-    manifest: true,
-    rollupOptions: {
-      input: 'src/main.jsx' // Adjust the path to your new entry point
-    }
+    outDir: '../dist'
   }
 });
