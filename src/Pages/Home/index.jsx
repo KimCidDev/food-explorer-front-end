9fb1fad47db091f6d33d09bcb3e2ce8333340305
Kim <cid.kim@gmail.com>
2024-03-23 05:32:01 -0600

    fix[home]: .banner.textContent className typo

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -143,7 +143,7 @@ export function Home() {
       {search === "" && (
         <div className="banner">
           <img src={macaroon} alt="blackberry ice cream bar" />
-          <div className={`textContent ${search === '' ? 'fadeInAnimation' : ''}`}>
+          <div className={`textContent ${search === '' ? 'fadeInAnim' : ''}`}>
             <h2>Why Wait for Special Days?</h2>
             <p>Treat yourself to mouthwatering delights anytime you want.</p>
           </div>
