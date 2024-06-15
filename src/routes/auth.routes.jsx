89d9cb394bb3d9c8b04a81cc1e879afcb726ac58
Kim <cid.kim@gmail.com>
2024-06-15 16:12:10 -0600

    fix[authroutes]: typo

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,12 +1,10 @@
 // src/routes/auth.routes.jsx
 
-import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { SignIn } from '../pages/SignIn';
 import { SignUp } from '../pages/SignUp';
 
 export function AuthRoutes() {
-  const navigate = useNavigate();
-
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
