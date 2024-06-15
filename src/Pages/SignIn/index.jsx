7673b874426077939ec47a99fcd440808c2f3c39
Kim <cid.kim@gmail.com>
2024-06-15 17:11:00 -0600

    refactor[sign in]: if loading, then only Breathing

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -31,32 +31,33 @@ export function SignIn() {
 
   return (
     <Container>
-      <Logo>
-        <svg width="20" height="44" viewBox="0 0 39 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M19.6574 0L38.4133 10.8287V32.4862L19.6574 43.3149L0.901548 32.4862V10.8287L19.6574 0Z" fill="#9C27B0" />
-        </svg>
-        <h1>FaveMeal</h1>
-      </Logo>
-
       {loading ? (
         <Breathing size="medium" />
       ) : (
-        <Form>
-          <h1>Sign In</h1>
-          <Input
-            title="Email"
-            placeholder="Enter your email address"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            title="Password"
-            inputType="password"
-            placeholder="Enter your password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button title="Sign In" onClick={handleSignIn} />
-          <ButtonText to='/register' title="Sign Up" />
-        </Form>
+        <>
+          <Logo>
+            <svg width="20" height="44" viewBox="0 0 39 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M19.6574 0L38.4133 10.8287V32.4862L19.6574 43.3149L0.901548 32.4862V10.8287L19.6574 0Z" fill="#9C27B0" />
+            </svg>
+            <h1>FaveMeal</h1>
+          </Logo>
+          <Form>
+            <h1>Sign In</h1>
+            <Input
+              title="Email"
+              placeholder="Enter your email address"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              title="Password"
+              inputType="password"
+              placeholder="Enter your password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button title="Sign In" onClick={handleSignIn} />
+            <ButtonText to='/register' title="Sign Up" />
+          </Form>
+        </>
       )}
     </Container>
   );
