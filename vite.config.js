efb63bd28e9052850ad3a16339355dbcf36d3e5f
Kim <cid.kim@gmail.com>
2024-06-16 13:24:13 -0600

    test[build edit]

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@stripe/stripe-js', '@stripe/react-stripe-js']
+  },
   build: {
     rollupOptions: {
       external: ['@stripe/stripe-js', '@stripe/react-stripe-js']
