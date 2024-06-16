ce0d0298998c7fc0be7d326e4749232ac43437a0
Kim <cid.kim@gmail.com>
2024-06-16 12:37:01 -0600

    teste[routes/app]

---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,23 @@
+// src/routes/app.routes.jsx
+
+import { Routes, Route } from 'react-router-dom';
+import { Home } from '../pages/Home'; // Ensure this path is correct
+import { Cart } from '../pages/Cart';
+import { FoodInfo } from '../pages/FoodInfo';
+import { NewDishAdmin } from '../pages/Admin/NewDishAdmin';
+import { EditDishAdmin } from '../pages/Admin/EditDishAdmin';
+import { useAuth } from '../hooks/auth';
+
+export function AppRoutes() {
+  const { user } = useAuth();
+
+  return (
+    <Routes>
+      {user.isAdmin && <Route path="/admin/newdish" element={<NewDishAdmin />} />}
+      {user.isAdmin && <Route path="/admin/editdish/:id" element={<EditDishAdmin />} />}
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/foodinfo/:id" element={<FoodInfo />} />
+    </Routes>
+  );
+}
