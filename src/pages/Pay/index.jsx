684d7d4a9a765c13597dc52f1745d754de34a0b9
Kim <cid.kim@gmail.com>
2024-06-17 02:20:23 -0600

    test[pay]: adapting the page to recieve the new .env values

--- src/pages/Pay/index.jsx
+++ src/pages/Pay/index.jsx
@@ -58,8 +58,7 @@ export function Pay() {
     }));
 
     try {
-      console.log('Stripe Public Key:', process.env.REACT_APP_STRIPE_PUBLIC_KEY);
-      const { data: { id } } = await axios.post(`${process.env.REACT_APP_API_URL}/payments/create-checkout-session`, {
+      const { data: { id } } = await axios.post(`${import.meta.env.VITE_API_URL}/payments/create-checkout-session`, {
         items,
       });
 
